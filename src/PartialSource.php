d204ec64c1a6b3035c1c1d4194d9de9b668ae2ba
Kevin Quillen <kevinquillen@gmail.com>
2025-12-10 11:57:51 -0500

    refactor: use composition instead of inheritance for PartialSource
    Twig\Source is final in Twig 3.x, so PartialSource now wraps
    Source with composition and delegates to it.

--- src/PartialSource.php
+++ src/PartialSource.php
@@ -6,18 +6,39 @@ namespace TwigPartials;
 
 use Twig\Source;
 
-class PartialSource extends Source
+class PartialSource
 {
+    private Source $source;
     private string $fragment;
 
     public function __construct(Source $source, string $fragment)
     {
-        parent::__construct($source->getCode(), $source->getName(), $source->getPath());
+        $this->source = $source;
         $this->fragment = $fragment;
+    }
+
+    public function getSource(): Source
+    {
+        return $this->source;
     }
 
     public function getFragment(): string
     {
         return $this->fragment;
+    }
+
+    public function getCode(): string
+    {
+        return $this->source->getCode();
+    }
+
+    public function getName(): string
+    {
+        return $this->source->getName();
+    }
+
+    public function getPath(): string
+    {
+        return $this->source->getPath();
     }
 }
