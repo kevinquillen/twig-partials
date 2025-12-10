bc1215eefbf62dbdd42f087dcf8cbe38a4d147f6
Kevin Quillen <kevinquillen@gmail.com>
2025-12-10 11:58:01 -0500

    refactor: add fragment registry to PartialLoader
    Store fragment identifiers in registry instead of returning PartialSource.
    Add helper methods: getFragment(), hasFragment(), parseTemplateName().

--- src/Loader/PartialLoader.php
+++ src/Loader/PartialLoader.php
@@ -6,9 +6,12 @@ namespace TwigPartials\Loader;
 
 use Twig\Loader\FilesystemLoader;
 use Twig\Source;
+use TwigPartials\PartialSource;
 
 class PartialLoader extends FilesystemLoader
 {
+    private array $fragmentRegistry = [];
+
     public function getSourceContext(string $name): Source
     {
         if (!str_contains($name, '#')) {
@@ -18,6 +21,27 @@ class PartialLoader extends FilesystemLoader
         [$file, $fragment] = explode('#', $name, 2);
         $source = parent::getSourceContext($file);
 
-        return new \TwigPartials\PartialSource($source, $fragment);
+        $this->fragmentRegistry[$name] = $fragment;
+
+        return new Source($source->getCode(), $name, $source->getPath());
+    }
+
+    public function getFragment(string $name): ?string
+    {
+        return $this->fragmentRegistry[$name] ?? null;
+    }
+
+    public function hasFragment(string $name): bool
+    {
+        return isset($this->fragmentRegistry[$name]);
+    }
+
+    public function parseTemplateName(string $name): array
+    {
+        if (!str_contains($name, '#')) {
+            return [$name, null];
+        }
+
+        return explode('#', $name, 2);
     }
 }
